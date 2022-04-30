119bff79da2ba5070d0276cd6ce559f433d577b5
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-29 21:03:57 -0300

    Rest Controller
    Criação da classe TesteController

---
+++ src/main/java/com/senaiIFit/senaIfit/controllers/TesteController.java
@@ -0,0 +1,16 @@
+package com.senaiIFit.senaIfit.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TesteController {
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String teste() {
+		return "Oi!";
+	}
+
+}

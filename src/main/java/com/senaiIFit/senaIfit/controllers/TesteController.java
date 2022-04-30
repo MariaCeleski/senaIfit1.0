32a9fa34e94fbac0fb6eafb7033b4b5684035f86
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-30 09:59:55 -0300

    Service
    Criando classe service

--- src/main/java/com/senaiIFit/senaIfit/controllers/TesteController.java
+++ src/main/java/com/senaiIFit/senaIfit/controllers/TesteController.java
@@ -14,9 +14,5 @@ public class TesteController {
 		return "Oi!Eu sou a primeira!";
 	}
 	
-	/*@GetMapping(value="/test")
-	public String teste2(@pathVariable("")) {
-		return "Abbreviate!";*/
-
-}*/
+	
 }

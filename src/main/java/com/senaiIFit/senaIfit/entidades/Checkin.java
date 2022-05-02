8be9cd257158534a4eaaf8caa472c30250aa84ff
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-05-02 15:34:13 -0300

    Update Checkin.java
    Ajustando data e hora

--- src/main/java/com/senaiIFit/senaIfit/entidades/Checkin.java
+++ src/main/java/com/senaiIFit/senaIfit/entidades/Checkin.java
@@ -5,52 +5,74 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Calendar;
+import java.time.LocalTime;
+import java.time.Period;
 import java.util.Date;
 
 public class Checkin {
-	
+
 	private Cliente cliente;
-	private Parceiro parceiros;
-	private LocalDateTime dataNascimento;
+	private Parceiro parceiro;
+	private LocalTime horaEntrada;
+	private LocalTime horaSaida;
+	private LocalDate dataNascimento;
 	private LocalDate dataAtual;
-	
-	
 	private Instrutor instrutor;
 	private ModalidadeAtividade atividade;
-	
+
 	public Checkin() {
-	this.dataNascimento = LocalDateTime.now();
-	this.dataAtual = LocalDate.now();
-	
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	Calendar cal = Calendar.getInstance();
-	
-	Date dataNascimento = sdf.parse(sc.next());{
-	
-	
-	cal.setTime(dataNascimento);
-	cal.add(Calendar.MONTH, 0);
-	
-	/*data actual descobrir data de nascimento*/
-	int year = cal.get(Calendar.YEAR);
-	dataNascimento = cal.getTime();
-	
-	Date atualData = new Date();
-	cal.setTime(atualData);
-	
-	int atualYear = cal.get(Calendar.YEAR);
-	int age = (atualYear - year);
-	}
-	
-	
+		this.dataAtual = LocalDate.now();
+		this.horaSaida = LocalTime.now();
+		this.horaEntrada = LocalTime.from(horaEntrada);
+		this.horaEntrada.format(null);
+		
 	}	
+		 
+		
+		//calcular idade
+		/*LocalDate dataNascimento = LocalDate.of(dataNascimento);
+		LocalDate hoje = LocalDate.now();
+		Period.between(dataNascimento, hoje);*/
 	
-	public LocalDateTime getDataNascimento() {
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Parceiro getParceiro() {
+		return parceiro;
+	}
+
+	public void setParceiro(Parceiro parceiro) {
+		this.parceiro = parceiro;
+	}
+
+	public LocalTime getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(LocalTime horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public LocalTime getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(LocalTime horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+	public LocalDate getDataNascimento() {
 		return dataNascimento;
 	}
 
-	public void setDataNascimento(LocalDateTime dataNascimento) {
+	public void setDataNascimento(LocalDate dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
 
@@ -61,36 +83,19 @@ public class Checkin {
 	public void setDataAtual(LocalDate dataAtual) {
 		this.dataAtual = dataAtual;
 	}
-		
-	public ModalidadeAtividade getAtividade() {
-		return atividade;
-	}
-	public void setAtividade(ModalidadeAtividade atividade) {
-		this.atividade = atividade;
-	}
-	public Cliente getCliente() {
-		return cliente;
-	}
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
-	public Parceiro getParceiros() {
-		return parceiros;
-	}
-	public void setParceiros(Parceiro parceiros) {
-		this.parceiros = parceiros;
-	}
-	public LocalDateTime getDataHora() {
-		return dataNascimento;
-	}
-	
-	
+
 	public Instrutor getInstrutor() {
 		return instrutor;
 	}
+
 	public void setInstrutor(Instrutor instrutor) {
 		this.instrutor = instrutor;
 	}
-	
 
-}
+	public ModalidadeAtividade getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(ModalidadeAtividade atividade) {
+		this.atividade = atividade;
+	}

ecf6e985cb2a92fa3f5fc5410e87a518e5aaa3a5
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-05-01 22:32:58 -0300

    Update Checkin.java

--- src/main/java/com/senaiIFit/senaIfit/entidades/Checkin.java
+++ src/main/java/com/senaiIFit/senaIfit/entidades/Checkin.java
@@ -1,20 +1,67 @@
 package com.senaiIFit.senaIfit.entidades;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Checkin {
 	
 	private Cliente cliente;
 	private Parceiro parceiros;
-	private LocalDateTime dataHora;
-	private String apelidoEndereco;
+	private LocalDateTime dataNascimento;
+	private LocalDate dataAtual;
+	
+	
 	private Instrutor instrutor;
 	private ModalidadeAtividade atividade;
 	
 	public Checkin() {
-	this.dataHora = LocalDateTime.now();
+	this.dataNascimento = LocalDateTime.now();
+	this.dataAtual = LocalDate.now();
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	Calendar cal = Calendar.getInstance();
+	
+	Date dataNascimento = sdf.parse(sc.next());{
+	
+	
+	cal.setTime(dataNascimento);
+	cal.add(Calendar.MONTH, 0);
+	
+	/*data actual descobrir data de nascimento*/
+	int year = cal.get(Calendar.YEAR);
+	dataNascimento = cal.getTime();
+	
+	Date atualData = new Date();
+	cal.setTime(atualData);
+	
+	int atualYear = cal.get(Calendar.YEAR);
+	int age = (atualYear - year);
+	}
+	
+	
 	}	
 	
+	public LocalDateTime getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDateTime dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public LocalDate getDataAtual() {
+		return dataAtual;
+	}
+
+	public void setDataAtual(LocalDate dataAtual) {
+		this.dataAtual = dataAtual;
+	}
+		
 	public ModalidadeAtividade getAtividade() {
 		return atividade;
 	}
@@ -34,15 +81,10 @@ public class Checkin {
 		this.parceiros = parceiros;
 	}
 	public LocalDateTime getDataHora() {
-		return dataHora;
+		return dataNascimento;
 	}
 	
-	public String getApelidoEndereco() {
-		return apelidoEndereco;
-	}
-	public void setApelidoEndereco(String apelidoEndereco) {
-		this.apelidoEndereco = apelidoEndereco;
-	}
+	
 	public Instrutor getInstrutor() {
 		return instrutor;
 	}
